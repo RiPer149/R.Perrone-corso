ae11c52a5d6b732e89f4573ba4342b6bea4190c5
RiPer149 <r.perrone149@gmail.com>
2024-11-13 14:18:52 +0100

    S2_L3

---
+++ S2_L3/Esercizio.py
@@ -0,0 +1,8 @@
+#Generatore di nomi band
+
+città = input("Inserisci il nome della tua città di origine ")
+pet = input("Inserisci il nome del tuo animale domestico ")
+
+nome_band = pet + città
+
+print(f"Il nome della tua band potrebbe essere: {nome_band}")
