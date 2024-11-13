7bab1a25e7a823609a27b8870aff7037f041d9d1
RiPer149 <r.perrone149@gmail.com>
2024-11-13 14:17:44 +0100

    Delete Esercizio.py

--- S2_L3/Esercizio.py
+++
@@ -1,8 +0,0 @@
-#Generatore di nomi band
-
-città = input("Inserisci il nome della tua città di origine ")
-pet = input("Inserisci il nome del tuo animale domestico ")
-
-nome_band = pet + città
-
-print(f"Il nome della tua band potrebbe essere: {nome_band}")
